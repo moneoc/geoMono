01800a1ece9881cff0abe8f251805bfe42f7664c
moneoc <moneoc@gmail.com>
2016-05-14 09:19:58 -0500

    14.05.16.2

--- js/evento.js
+++ js/evento.js
@@ -10,8 +10,8 @@ var localizame ={
     	var lon = position.coords.longitude;
     	var latlng = new google.maps.LatLng(lat, lon);
 
-	    geocoder = new google.maps.Geocoder();
-	    geocoder.geocode({ "latLng": latlng }, function (results, status) {
+	    //geocoder = new google.maps.Geocoder();
+	    //geocoder.geocode({ "latLng": latlng }, function (results, status) {
 
 	        var mapaPersonalizado = {
 	            zoom: 14,
@@ -36,10 +36,30 @@ var localizame ={
 	            icon: 'js/img/icon.png'
 	        });
 
-	    });
+	   //});
 	},
-	correEnCirculos: function(errorch){
-		alert("Tarde o temprano te encontraré ¬_¬. " + errorch.message);
-	}
+	correEnCirculos: function(error){
+    switch (error.code) {
+        case error.PERMISSION_DENIED:
+            //swal("¡ERROR!", "No se tiene permiso para obtener su ubicación", "error");
+            alert('ERROR: User denied access to track physical position!');
+            break;
+
+        case error.POSITION_UNAVAILABLE:
+            //swal("¡ERROR!", "Problema al intentar obtener su localización", "error");
+            alert("ERROR: There is a problem getting the position of the device!");
+            break;
+
+        case error.TIMEOUT:
+            //swal("¡ERROR!", "Se ha agotado el tiempo de espera para obtener su localización", "error");
+            alert("ERROR: The application timed out trying to get the position of the device!");
+            break;
+
+        default:
+            //swal("¡ERROR!", "Problema desconocido", "error");
+            alert("ERROR:  Unknown problem!");
+            break;
+    }
+}
 };
 $(localizame.inicio);
