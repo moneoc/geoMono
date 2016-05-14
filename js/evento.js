c7e2c337880c4e4c6c80520af0005f00a535387f
moneoc <moneoc@gmail.com>
2016-05-14 10:31:53 -0500

    14.05.16.8

--- js/evento.js
+++ js/evento.js
@@ -14,12 +14,20 @@ var fn = {
 	dibujarMapa: function(lat, lng) {
 		//Posición del mapa
 		var latlng = new google.maps.LatLng(lat, lng);
-		var myOptions = {
-			zoom: 16,
-			center: latlng,
-			mapTypeId: google.maps.MapTypeId.ROADMAP
-		};
-		var map = new google.maps.Map(document.getElementById("map_canvas"),myOptions);
+		var confGoogleMaps = {
+            zoom: 14,
+            center: latlng,
+            mapTypeControl: false,
+            scrollwheel: false,
+            draggable: true,
+            scaleControl: false,
+            zoomControl: false,
+            disableDefaultUI: true,
+            disableDoubleClickZoom: true,
+            navigationControlOptions: { style: google.maps.NavigationControlStyle.SMALL },
+            mapTypeId: google.maps.MapTypeId.TERRAIN
+        };
+		var map = new google.maps.Map(document.getElementById("map_canvas"),confGoogleMaps);
 		//Marcador
 		var marker = new google.maps.Marker({
 			position: latlng, 
